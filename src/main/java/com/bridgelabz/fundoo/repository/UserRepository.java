c3874d66366ff9d0fb3fac96730e7fcd2a5c6589
P J Mugilan <pjmugilan@gmail.com>
2026-07-27 21:39:47 +0530

    feat(user): add pageable repository method

--- src/main/java/com/bridgelabz/fundoo/repository/UserRepository.java
+++ src/main/java/com/bridgelabz/fundoo/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.bridgelabz.fundoo.repository;
 
 import com.bridgelabz.fundoo.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
+
+    Page<User> findByDeletedFalse(Pageable pageable);
 }
