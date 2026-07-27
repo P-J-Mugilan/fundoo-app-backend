7c4307e444839b1a3fef284ced2b1218424905e1
P J Mugilan <pjmugilan@gmail.com>
2026-07-27 21:41:53 +0530

    refactor(user): update service for pagination

--- src/main/java/com/bridgelabz/fundoo/service/UserService.java
+++ src/main/java/com/bridgelabz/fundoo/service/UserService.java
@@ -6,6 +6,7 @@ import com.bridgelabz.fundoo.entity.User;
 import com.bridgelabz.fundoo.exception.EmailAlreadyExistsException;
 import com.bridgelabz.fundoo.exception.ResourceNotFoundException;
 import com.bridgelabz.fundoo.exception.UserNotFoundException;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -13,7 +14,14 @@ public interface UserService {
     UserResponseDto registerUser(RegisterRequestDto registerRequest) throws EmailAlreadyExistsException;
     AuthResponseDto loginUser(LoginRequestDto loginRequest) throws UserNotFoundException;
     UserResponseDto getUserById(Long id) throws UserNotFoundException;
-    List<UserResponseDto> getAllUsers();
+//    List<UserResponseDto> getAllUsers();
+
+    Page<UserResponseDto> getAllUsers(
+            int page,
+            int size,
+            String sortBy,
+            String direction
+    );
     UserResponseDto updateUser(Long id, RegisterRequestDto updateRequest) throws UserNotFoundException;
     UserResponseDto updateUserRole(Long id, com.bridgelabz.fundoo.entity.enums.Role role) throws UserNotFoundException;
     void deleteUser(Long id) throws UserNotFoundException;
